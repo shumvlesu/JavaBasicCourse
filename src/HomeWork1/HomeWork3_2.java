3dc3a244d2f760000e5cf14ba599e7d3e5d8268f
sergey <sashumihin@gmail.com>
2020-12-07 08:29:28 +0300

    дз3

---
+++ src/HomeWork1/HomeWork3_2.java
@@ -0,0 +1,32 @@
+package HomeWork1;
+
+public class HomeWork3_2 {
+
+    public static void main(String[] args) {
+
+        //2 * Создать массив из слов String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        //При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя,
+        //сравнивает его с загаданным словом и сообщает правильно ли ответил пользователь. Если слово не угадано, компьютер показывает буквы которые стоят на своих местах.
+        //apple – загаданное
+        //apricot - ответ игрока
+        //ap############# (15 символов, чтобы пользователь не мог узнать длину слова)
+        //Для сравнения двух слов посимвольно, можно пользоваться:
+        //String str = "apple";
+        //str.charAt(0); - метод, вернет char, который стоит в слове str на первой позиции
+        //Играем до тех пор, пока игрок не отгадает слово
+        //Используем только маленькие буквы
+
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        String[] mask = new String[15];
+        System.out.println(chooseAWord(words));
+
+    }
+
+    private static String chooseAWord(String[] words) {
+
+        return words[(int) (Math.random()*words.length)];
+
+    }
+
+
+}
