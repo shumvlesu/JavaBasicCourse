9094a40d045d4dd1db760a6a1b3a0eb2925875ca
shumvlesu <sashumihin@gmail.com>
2020-12-09 18:56:23 +0300

    4 урок

--- src/Lessons/Lesson4.java
+++ src/Lessons/Lesson4.java
@@ -1,4 +1,34 @@
 package Lessons;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Lesson4 {
+
+  static final int SIZE = 3;
+
+  static final char DOT_EMPTY = '•';
+  static final char DOT_HUMAN = 'X';
+  static final char DOT_AI = '0';
+
+  static final char HEADER_FIRST_SYMBOL = '♥';
+  static final String EMPTY = " ";
+
+  static final char[][] map = new char[SIZE][SIZE];
+  static final Scanner in = new Scanner(System.in);
+  static final Random random = new Random();
+
+  public static void main(String[] args) {
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        map[i][j] = DOT_EMPTY;
+      }
+    }
+    System.out.print(HEADER_FIRST_SYMBOL+EMPTY);
+    for (int i = 0; i < SIZE; i++) {
+      System.out.print(i+1+EMPTY);
+    }
+  }
+
+
 }
