25ebc4469143ac751c7c1663b8e93179eca9c34b
shumvlesu <sashumihin@gmail.com>
2020-12-11 18:29:19 +0300

    4 урок

--- src/Lessons/Lesson4.java
+++ src/Lessons/Lesson4.java
@@ -13,6 +13,7 @@ public class Lesson4 {
 
   static final char HEADER_FIRST_SYMBOL = '♥';
   static final String EMPTY = " ";
+  static int turnsCount;
 
   static final char[][] map = new char[SIZE][SIZE];
   static final Scanner in = new Scanner(System.in);
@@ -29,6 +30,7 @@ public class Lesson4 {
   }
 
   private static void printMap() {
+    System.out.println();
     printHeaderMap();
     printBodyMap();
   }
@@ -64,22 +66,32 @@ public class Lesson4 {
   }
 
   private static void playGame() {
-    //ход человека
-    humanYurn();
-    printMap();
-    //проверка на окончание игры после хода человека
+    turnsCount = 0;
+    while (true) {
+      //ход человека
+      humanYurn();
+      printMap();
+      //проверка на окончание игры после хода человека
+      checkEnd(DOT_HUMAN);
 
-
-    //ход ии
-    printMap();
-    //проверка на окончание игры после хода ии
+      //ход ии
+      aiTurn();
+      printMap();
+      //проверка на окончание игры после хода ии
+      checkEnd(DOT_AI);
+    }
   }
 
   private static void humanYurn() {
-    int rowNumber = 0;
-    int columnNumber = 0;
+    int rowNumber;
+    int columnNumber;
 
+    System.out.println("\n  Ход человека! Введите номера строки и столбца");
     do {
+      rowNumber = 0;
+      columnNumber = 0;
+
+      System.out.print("Строка = ");
       if (in.hasNextInt()) {
         rowNumber = in.nextInt() - 1;
       } else {
@@ -88,30 +100,100 @@ public class Lesson4 {
         continue;
       }
 
+      System.out.print("Столбец = ");
       if (in.hasNextInt()) {
         columnNumber = in.nextInt() - 1;
       } else {
         in.next();
-        System.out.println("Введите число!");
+        // System.out.println("Введите число!");
         continue;
       }
-    } while (!isHumanValidTurn(rowNumber,columnNumber));
+    } while (!isHumanValidTurn(rowNumber, columnNumber));
 
     map[rowNumber][columnNumber] = DOT_HUMAN;
 
   }
 
-  private static boolean isHumanValidTurn(int rowNumber,int columnNumber) {
-    return isNumbersValued(rowNumber,columnNumber) && isCellOccupancy(rowNumber,columnNumber);
+  private static boolean isHumanValidTurn(int rowNumber, int columnNumber) {
+    return isNumbersValued(rowNumber, columnNumber) && isCellOccupancy(rowNumber, columnNumber, false);
   }
 
+
   //валидность чисел
-  private static boolean isNumbersValued(int rowNumber,int columnNumber) {
+  private static boolean isNumbersValued(int rowNumber, int columnNumber) {
+    if (rowNumber > SIZE || rowNumber < 0 || columnNumber > SIZE || columnNumber < 0) {
+      System.out.println("\nПроверьте значение строки и столбца");
+      return false;
+    }
+    return true;
   }
 
   //проверка на занятость
-  private static boolean isCellOccupancy(int rowNumber,int columnNumber) {
+  private static boolean isCellOccupancy(int rowNumber, int columnNumber, boolean isAI) {
+    if (map[rowNumber][columnNumber] != DOT_EMPTY) {
+      if (!isAI) System.out.println("\nВы выбрали занятую ячейку!");
+      return false;
+    }
+    return true;
   }
 
+  private static void checkEnd(char symbol) {
+    boolean isEnd = false;
+
+    //победа
+    if (checkWin(symbol)) {
+      isEnd = true;
+      System.out.println();
+      String winMessage;
+      if (symbol == DOT_HUMAN) {
+        winMessage = "Ура! Вы победили!";
+      } else {
+        winMessage = "Восстание близко. ИИ победил.";
+      }
+
+      System.out.println(winMessage);
+    }
+
+
+    //ничья
+    if (!isEnd && isMapFullByTurnsCount()) {
+      System.out.println("Ничья!");
+      isEnd = true;
+    }
+
+    if (isEnd) System.exit(0);
+
+  }
+
+  private static boolean isMapFullByTurnsCount() {
+    return ++turnsCount == SIZE * SIZE;
+  }
+
+  private static boolean checkWin(char symbol) {
+
+    if (map[0][0] == symbol && map[0][1] == symbol && map[0][2] == symbol) return true;
+    if (map[1][0] == symbol && map[1][1] == symbol && map[1][2] == symbol) return true;
+    if (map[2][0] == symbol && map[2][1] == symbol && map[2][2] == symbol) return true;
+
+    if (map[0][0] == symbol && map[1][0] == symbol && map[2][0] == symbol) return true;
+    if (map[0][1] == symbol && map[1][1] == symbol && map[2][1] == symbol) return true;
+    if (map[0][2] == symbol && map[1][2] == symbol && map[2][2] == symbol) return true;
+
+    if (map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol) return true;
+    if (map[0][2] == symbol && map[1][1] == symbol && map[2][0] == symbol) return true;
+
+    return false;
+  }
+
+  private static void aiTurn() {
+    int rowNumber;
+    int columnNumber;
+    System.out.println("\nХод копьютера");
+    do {
+      rowNumber = random.nextInt(SIZE);
+      columnNumber = random.nextInt(SIZE);
+    } while (!isCellOccupancy(rowNumber, columnNumber, true));
+    map[rowNumber][columnNumber] = DOT_AI;
+  }
 
 }
