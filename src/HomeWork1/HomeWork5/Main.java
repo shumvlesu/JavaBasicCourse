ade9f4fb3205638d881f7aaed4984a0e821e359d
shumvlesu <sashumihin@gmail.com>
2020-12-14 15:46:52 +0300

    ДЗ 5 урок

--- src/HomeWork1/HomeWork5/Main.java
+++ src/HomeWork1/HomeWork5/Main.java
@@ -1,4 +1,32 @@
 package HomeWork1.HomeWork5;
 
 public class Main {
+
+  public static void main(String[] args) {
+    //* Создать массив из 5 сотрудников
+    //Пример:
+    //Person[] persArray = new Person[5]; // Вначале объявляем массив объектов
+    //persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30); // потом для каждой ячейки массива задаем объект
+    //persArray[1] = new Person(...);
+    //...
+    //persArray[4] = new Person(...);
+    //
+    Person[] persArray = new Person[5];
+    persArray[0] = new Person("Иванов Иван Иванович", "Инженер", "ivivan@mailbox.com", "892312312", 30000, 30);
+    persArray[1] = new Person("Петров Петр Петрович", "Сантехник", "", "891845524", 20000, 34);
+    persArray[2] = new Person("Попкова Аида Вячеславовна", "Клинниг мастер", "", "8928555666", 0, 42);
+    persArray[3] = new Person("Супрунов Валерий Николаевич", "Разнорабочий", "ааа@mail.com", "891855512", 25000, 52);
+    persArray[4] = new Person("Соломахина Татьяна Михайловна", "Повар", "", "888812312", 60000, 30);
+
+    //* С помощью цикла вывести информацию только о сотрудниках старше 40 лет;
+    for (int i = 0; i < persArray.length; i++) {
+      if (persArray[i].getAge()>=40){
+        System.out.println(persArray[i].toString());
+      }
+    }
+
+
+
+  }
+
 }
--- src/HomeWork1/HomeWork5/Person.java
+++ src/HomeWork1/HomeWork5/Person.java
@@ -1,4 +1,70 @@
 package HomeWork1.HomeWork5;
 
 public class Person {
+
+
+  //* Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст;
+  protected String FIO;
+  protected String position;
+  protected String email;
+  protected String tel;
+  private int salary;
+  private int age;
+  protected final int MINIMAL_SALARY = 12130; //минималка в краснодарском крае :)
+
+  //* Конструктор класса должен заполнять эти поля при создании объекта;
+  public Person(String FIO,
+                String position,
+                String email,
+                String tel,
+                int salary,
+                int age){
+
+    this.FIO = FIO;
+    this.position = position;
+    this.email = email;
+    this.tel = tel;
+    setSalary(salary);
+    setAge(age);
+  }
+
+  public Person(String FIO){
+    this(FIO,"","","",0 ,0);
+  }
+
+  //* Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль;
+  @Override
+  public String toString(){
+    return String.format("Это %s, должность %s с зарплатой: %d, email-%s  возраст %d. Телефон:%s", FIO, position, getSalary(), email, getAge(),tel);
+  }
+
+  public int getSalary() {
+    if (salary == 0) {
+      return MINIMAL_SALARY;
+    }else {
+      return salary;
+    }
+  }
+
+  public void setSalary(int salary) {
+    if(salary>0) {
+      this.salary = salary;
+    }else if (salary==0){
+      this.salary = MINIMAL_SALARY;
+    } else {
+      System.err.println("Нельзя у человека брать деньги за работу!");
+    }
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    if(age>=0) {
+      this.age = age;
+    }else {
+      System.err.println("Не может быть отрицательного возраста.");
+    }
+  }
 }
