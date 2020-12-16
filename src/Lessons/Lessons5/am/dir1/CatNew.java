b45782d51b4d33da9351b5470c7006e9279205ee
sergey <sashumihin@gmail.com>
2020-12-16 21:11:07 +0300

    5 урок

---
+++ src/Lessons/Lessons5/am/dir1/CatNew.java
@@ -0,0 +1,19 @@
+package Lessons.Lessons5.am.dir1;
+
+public class CatNew {
+
+    public String name;
+    String color;
+    protected int age;
+    private int weight;
+
+    public static void main(String[] args) {
+        CatNew catNew = new CatNew();
+
+        catNew.name = "111";
+        catNew.color = "222";
+        catNew.age = 333;
+        catNew.weight = 444;
+    }
+
+}
