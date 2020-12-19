ef38e0773e67eb7a19c062d138bfd82a8f21550d
shumvlesu <sashumihin@gmail.com>
2020-12-19 17:40:58 +0300

    7 урок 1:34:46

--- src/Lessons/Lesson7/StringConcat.java
+++ src/Lessons/Lesson7/StringConcat.java
@@ -17,6 +17,8 @@ public class StringConcat {
 
     String c = "(" + 2 + 3 + ")" + b3 + "!";
 
+
+    System.out.println("Вывод строк символами или отдельными словами:");
     //разбиваем строку на символы
     for (char ch : b1.toCharArray()) {  //toCharArray() разбивает строку на массив символов.
       System.out.println("Буква: " + ch);
@@ -28,24 +30,52 @@ public class StringConcat {
       System.out.println("Слово: " + word);
     }
 
+
+
+
+    System.out.println("Сравнивание строк:");
     System.out.println(a1.equals(a2)); //сравнивание именно значений
     System.out.println(a1.compareTo(a2)); //compareTo применяется обычно для сортировки. Тут будет возвращено 4 так как
     //первый символ в строке ("H") на 4 символа дальше первого символа во второй строке("D")
     //Получается если число положительное то значит строка а2 должна быть выше в сортировки строки а1
     //а если отрицательное то а1 должна быть выше а2 в сортировке
 
+
+
+
+    System.out.println("Проверка на пустату строк:");
     String space = " ";
     System.out.println(space == null);
-    System.out.println(space.isEmpty());
-    System.out.println(space.isBlank());
+    System.out.println(space.isEmpty());//скажет что строка не пустая так как в строке есть пробел. и вернет false
+    System.out.println(space.isBlank());//скажет что строка пустая (true) так как isBlank проверяет на истинно пустоту строки.
 
+
+
+
+    System.out.println("Выделение(копирование) подстроки из строки:");
     System.out.println(a3.substring(6));
-    System.out.println(a3.substring(6, 16));
+    System.out.println(a3.substring(6, 16)); //1 параметр это начало откуда выделяем , второй параметр это если нужно окончание выделения
 
+
+
+    
+    System.out.println("Замена подстрокой в строке:");
+    System.out.println(a3.replace("My", "Our")); //Заменяем все "My" на "Our" в строке а3 аналог стрЗаменить в 1с
     System.out.println(a3.replaceAll("My", "Our"));
+    System.out.println(a3.replace("l", "!")); //Заменяем все "My" на "Our" в строке а3 аналог стрЗаменить в 1с
+    System.out.println(a3.replaceAll("l", "!"));
 
-    System.out.println(a3.indexOf(' '));
-    System.out.println(a3.charAt(16));
+
+
+
+    System.out.println("Нахождение индекса символа в строке:");
+    System.out.println(a3.indexOf(' '));//Узнаем какой индекс в строке имеет пробел
+    System.out.println(a3.charAt(16));//Узнаем какой символ соответствует 16 индексу в строке
+
+
+
+
+
 
 //        File file = new File("X:\\Ya.Disk\\work\\Lessons\\java_1\\src\\lesson7\\string\\resources\\text.txt");
     var file = new File("src/lesson7/string/resources/java_platform.txt");
